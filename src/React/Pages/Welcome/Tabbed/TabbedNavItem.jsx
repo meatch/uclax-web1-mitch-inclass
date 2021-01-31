ac57ec7c271e1102350f82f3f451c31d6408d265
Mitchell Gohman <meatch@me.com>
2021-01-31 10:11:34 -0800

    Added initital Tabbed Content

---
+++ src/React/Pages/Welcome/Tabbed/TabbedNavItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavTabbedItem = () => {
+
+    return (
+        <NavTabbedItemStyled className='NavTabbedItem'>
+            NavTabbedItem 
+        </NavTabbedItemStyled>
+    );
+}
+
+export default NavTabbedItem;
+
+const NavTabbedItemStyled = styled.div`
+    
+`;
